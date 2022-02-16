edf83d75c5a2abf082aa79da88c6926a4ee06f8f
alexis muñoz <munozmiguelalexis@gmail.com>
2022-02-16 11:21:50 -0300

    add file-loader configuration in webpack

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import "./layout.css";
+import css from "./layout.css";
+import { Header } from "components/header";
 
 function Layout() {
    return (
       <div>
+         <Header></Header>
          <Outlet />
       </div>
    );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { TextField } from "ui/text-field";
 import { MainButton } from "ui/buttons";
+import { Link } from "react-router-dom";
 
 function Home() {
    return (
       <div>
-         <h1>Hacé tu busqueda</h1>
-         <TextField type="text" placeholder="Buscar" />
-         <MainButton>Buscar</MainButton>
+         <h1>Mascotas perdidas cerca tuyo</h1>
+         <p>
+            Para ver las mascotas reportadas cerca tuyo necesitamos permiso para conocer
+            tu ubicación.
+         </p>
+
+         <MainButton>Dar mi ubicación</MainButton>
       </div>
    );
 }
